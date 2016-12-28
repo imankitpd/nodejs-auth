e4510e591ec2312b604ff93d4cb6e1bba044fa04
Ankit prasad <imankitpd@gmail.com>
2016-12-28 20:03:58 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -37,13 +37,31 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send();
     }
 
-    Todo.findById({_id: id}).then((todo) => {
+    Todo.findById(id).then((todo) => {
         if (!todo) {
             return res.status(404).send(todo);
         }
 
         res.send({todo});
         }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!id) {
+            return res.status(404).send();
+        }
+
+        res.send(todo);
+    }).catch((e) => {
         res.status(400).send();
     });
 });
