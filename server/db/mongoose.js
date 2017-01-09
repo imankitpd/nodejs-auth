90a42d10340a9313a3f144807e49ebf46f8f9bd9
Ankit prasad <imankitpd@gmail.com>
2017-01-09 22:19:25 +0530

    Update the db object in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,8 @@ mongoose.Promise = global.Promise;
 
 let db = {
     localhost: 'mongodb://localhost:27017/EventManagement',
-    // mlab: 'mongodb://<ankitPrasad>:<Ankit@123>@ds159328.mlab.com:59328/event-management'
-    mlab: 'mongodb://<dbuser>:<dbpassword>@ds159328.mlab.com:59328/event-management'
+    mlab: 'mongodb://ankitPrasad:Ankit@123@ds159328.mlab.com:59328/event-management'
+    // mlab: 'mongodb://<dbuser>:<dbpassword>@ds159328.mlab.com:59328/event-management'
 }
 
 mongoose.connect( db.localhost || db.mlab);
@@ -13,3 +13,5 @@ mongoose.connect( db.localhost || db.mlab);
 // mongoose.connect('mongodb://localhost:27017/EventApp');
 
 module.exports = {mongoose};
+
+
